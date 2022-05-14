4f27e12b9aeb133de526a51f29ea3739f28c76ca
danielss_stef@yahoo.com <danielss_stef@yahoo.com>
2022-05-14 17:03:39 +0300

    bottom navigation bar

--- app/src/main/java/com/example/tutorialkotlin/MainActivity.kt
+++ app/src/main/java/com/example/tutorialkotlin/MainActivity.kt
@@ -7,22 +7,39 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.widget.SearchView
 import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import com.example.tutorialkotlin.databinding.ActivityMainBinding
+import com.example.tutorialkotlin.fragments.DashboardFragment
+import com.example.tutorialkotlin.fragments.InfoFragment
+import com.example.tutorialkotlin.fragments.SettingsFragment
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var todoAdapter: TodoAdapter
-    private lateinit var binding: ActivityMainBinding
+
+    private val dashboardFragment = DashboardFragment();
+    private val settingsFragment = SettingsFragment();
+    private val infoFragment = InfoFragment();
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(settingsFragment)
         todoAdapter = TodoAdapter(mutableListOf(), mutableListOf())
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_dashboard -> replaceFragment(dashboardFragment)
+                R.id.ic_settings -> replaceFragment(settingsFragment)
+                R.id.ic_info -> replaceFragment(infoFragment)
+            }
+            true
+        }
 
         rvTodoItems.adapter = todoAdapter
         rvTodoItems.layoutManager = LinearLayoutManager(this)
@@ -59,6 +76,14 @@ class MainActivity : AppCompatActivity() {
 
         btnDeleteDoneTodos.setOnClickListener {
             todoAdapter.deleteDoneTodos()
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
         }
     }
 
